e28bdc29f06286f32f4dc6a519f8f991754cb946
jongc <105310191+jongchan159@users.noreply.github.com>
2025-07-24 20:19:44 +0900

    Update 오민식의 고민.cc

--- 백준/Platinum/1219. 오민식의 고민/오민식의 고민.cc
+++ 백준/Platinum/1219. 오민식의 고민/오민식의 고민.cc
@@ -22,49 +22,64 @@ void dfs(int u) {
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
 
-    cin >> N >> src >> dst >> M;
+    // int N, src, dst, M;
 
-    // 그래프 입력
-    for(int i = 0; i < M; i++) {
+    cin >> N >> src >> dst >> M; // N == Vertex, M == Edge
+
+    // init graph and dist array -> 여기는 꼭 암기
+    // vector<tuple<int, int, int>> graph; // u->v, cost(weight)
+
+    // input edge info
+    for(int i=0; i<M; i++){
         int u, v, cost;
         cin >> u >> v >> cost;
         graph.push_back({u, v, cost});
-        adj[u].push_back(v); // 정방향 인접 리스트 구성
+        adj[u].push_back(v); // *정방향 인접 리스트 구성
     }
 
+    // init & input profit -> weight = profit - cost (이후 연산에서만 활용해보자)
     int profit[N];
-    for(int i = 0; i < N; i++) {
+    for(int i=0; i<N; i++){
         cin >> profit[i];
     }
 
-    long long total[N];
+    long long total[N]; // total money (src -> all v)
     fill(total, total + N, MIN_LL);
     total[src] = profit[src];
 
-    // 벨만 포드: N-1번 relax
-    for(int i = 0; i < N - 1; i++) {
-        for(auto edge : graph) {
+    // Relaxation, src부터 시작
+    for(int i=0; i<N-1; i++){ // 최단거리의 조건은 V-1
+        // u까지의 거리 + w가 v까지의 현재 거리보다 작으면 갱신
+        for(auto edge : graph){
+            // int u = get<0>(edge);
+            // int v = get<1>(edge);
+            // int w = profit[v] - get<2>(edge);
             int u, v, cost;
             tie(u, v, cost) = edge;
             int w = profit[v] - cost;
-            if (total[u] != MIN_LL && total[u] + w > total[v]) {
+            if(total[u] != MIN_LL && total[u] + w > total[v]) { 
+                // printf("%d->%d, update total[%d]: %d -> %d\n",u, v, v, total[v], total[u] + w);
                 total[v] = total[u] + w;
             }
         }
     }
 
-    if (total[dst] == MIN_LL) {
-        cout << "gg\n";
+    // can't visit -> gg
+    // printf("total[dst:%d] = %lld\n", dst, total[dst]);
+    if(total[dst] == MIN_LL) {
+        cout << "gg" << endl;
         return 0;
     }
 
-    // 음수 사이클 여부 + DFS를 통한 도착지 영향 판별
-    for (auto edge : graph) {
+    // detect infinite cycle -> gee
+    for(auto edge : graph){
+        // int u = get<0>(edge);
+        // int v = get<1>(edge);
+        // int w = profit[v] - get<2>(edge);
         int u, v, cost;
         tie(u, v, cost) = edge;
         int w = profit[v] - cost;
-        if (total[u] != MIN_LL && total[u] + w > total[v]) {
-            // 사이클이 감지된 정점에서 DFS 시도
+        if(total[u] != MIN_LL && total[u] + w > total[v]) {
             fill(visited, visited + N, false);
             reachable = false;
             dfs(v);
@@ -75,6 +90,6 @@ int main() {
         }
     }
 
-    cout << total[dst] << '\n';
+    cout << total[dst] << endl;
     return 0;
 }
