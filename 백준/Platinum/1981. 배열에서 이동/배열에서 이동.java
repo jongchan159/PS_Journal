307c2f73a2d0e582df35ebfa9034760120206770
jongc <105310191+jongchan159@users.noreply.github.com>
2025-08-11 18:10:06 +0900

    [Platinum V] Title: 배열에서 이동, Time: 484 ms, Memory: 164384 KB -BaekjoonHub

---
+++ 백준/Platinum/1981. 배열에서 이동/배열에서 이동.java
@@ -0,0 +1,104 @@
+import java.io.*;
+import java.util.*;
+
+// 다익스트라
+// 인접리스트
+public class Main
+{
+    static int n, min = 200, max = 0, result = 200;
+    static int[][] map;
+    static int[] dy = {-1, 1, 0, 0}, dx = {0, 0, -1, 1}; // 상하좌우
+    
+    public static void main(String[] args) throws Exception { 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = null;
+
+        n = Integer.parseInt(br.readLine());
+        
+        map = new int[n][n];
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++) {
+                int v = Integer.parseInt(st.nextToken());
+                map[i][j] = v;
+                min = Math.min(v, min);
+                max = Math.max(v, max);
+            }
+        }
+        
+        // System.out.println("max = " + max + " min = " + min);
+        // binary search
+        result = max - min;
+        binarySearch(0, max - min);
+
+        bw.write(Integer.toString(result));
+        bw.close();
+    }
+
+    // 차잇값에 대해 이분탐색
+    static void binarySearch(int s, int e) {
+        while(s <= e) {
+            int mid = (s+e) / 2;
+            // System.out.println("\ncur mid = " + mid);
+            // BFS 성공 -> 더 낮은값 확인하기
+            if(bfs(mid)) {
+                result = Math.min(result, mid);
+                e = mid - 1;
+            }
+            // 실패 시 높은 값으로 다시 탐색
+            else {
+                s = mid + 1;
+            }
+        } 
+
+        return;
+    }
+
+    static boolean bfs(int gap) {
+        int s = min, e = s + gap;
+        
+        while(e <= max) {
+            if(map[0][0] < s || map[0][0] > e) {
+                s++; e++;
+                continue;
+            }
+            // System.out.println("s = " + s + " e = " + e);
+            ArrayDeque<int[]> dq = new ArrayDeque<>(); // (y,x)
+            boolean[][] visited = new boolean[n][n];
+            
+            dq.offer(new int[] {0,0});
+            visited[0][0] = true;
+            
+            while(!dq.isEmpty()) {
+                int[] cur = dq.poll();
+                int cy = cur[0];
+                int cx = cur[1];
+                // System.out.println("cur x = " + cx + " cur y = " + cy);
+                
+                for(int d=0; d<4; d++) {
+                    int ny = cy + dy[d], nx = cx + dx[d];
+                    if(ny < 0 || ny > n-1 || nx < 0 || nx > n-1) continue; // 지도 범위 벗어남
+                    if(visited[ny][nx]) continue; // 이미 방문 or 최대값 체크
+                    // 최대값보다 크거나 최소값보다 작으면 방문한것처럼 표시
+                    if(map[ny][nx] > e || map[ny][nx] < s) {
+                        visited[ny][nx] = true;
+                        continue;
+                    }
+                    // 다음 지역이 도착지
+                    if(ny == n-1 && nx == n-1) {
+                        // System.out.println("success");
+                        return true;
+                    }
+                    
+                    visited[ny][nx] = true;
+                    dq.offer(new int[] {ny, nx});
+                }
+            }
+            
+            s++; e++;
+        }
+        
+        return false;        
+    }
+}
